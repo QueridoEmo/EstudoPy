9e44df04abe95a50ebd88006597d6acc0aaeb02a
Sergio Mousquer <querido.exe@gmail.com>
2023-03-25 06:43:30 -0300

    + 2 ;D

---
+++ PROGRAMINEAS/Exercicio6.py
@@ -0,0 +1,4 @@
+segundos_str = input('Quantos segundos desejas calcular em Min e Hrs: ')
+total_segs = int(segundos_str)
+minutos = total_segs
+
---
+++ PROGRAMINEAS/Exerciocio7.py
@@ -0,0 +1,9 @@
+m_Conv = int (input('Quantos metros deseja calcular? '))
+dam_conv = int(m_Conv) / 10
+hm_conv = int(dam_conv) / 10
+km_conv = int(hm_conv) / 10
+#
+dm_conv = int(dam_conv) * 10
+cm_conv = int(dm_conv) * 10
+mm_conv = int(cm_conv) * 10
+print (f'{m_Conv}M equivale a:\n Km: {km_conv}\n Hm: {hm_conv}\n Dam: {dam_conv}\n Dm: {dm_conv}\n Cm: {cm_conv}\n Mm: {mm_conv}\n :D')
