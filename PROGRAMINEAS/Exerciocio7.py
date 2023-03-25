dfb4b4d30fc077cde5d25855dfee301c6fbec64e
Sergio Mousquer <querido.exe@gmail.com>
2023-03-25 06:46:25 -0300

    Add mais exercicios concluidos

--- PROGRAMINEAS/Exerciocio7.py
+++ PROGRAMINEAS/Exerciocio7.py
@@ -6,4 +6,4 @@ km_conv = int(hm_conv) / 10
 dm_conv = int(dam_conv) * 10
 cm_conv = int(dm_conv) * 10
 mm_conv = int(cm_conv) * 10
-print (f'{m_Conv}M equivale a:\n Km: {km_conv}\n Hm: {hm_conv}\n Dam: {dam_conv}\n Dm: {dm_conv}\n Cm: {cm_conv}\n Mm: {mm_conv}\n :D')
+print (f'{m_Conv}M equivale a:\n Km: {km_conv}\n Hm: {hm_conv}\n Dam: {dam_conv}\n Dm: {dm_conv}\n Cm: {cm_conv}\n Mm: {mm_conv}\n :D!')
