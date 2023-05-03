e342ea3f796077dc1db52e1d4d63a60d1123979f
Sergio Mousquer <querido.exe@gmail.com>
2023-05-03 20:31:55 -0300

    at game

---
+++ game/game.py
@@ -0,0 +1,40 @@
+import pygame
+
+pygame.init()
+x = 400
+y = 300
+velocidade_objeto1 = 10
+tela_fundo1 = pygame.image.load('game/estrada.png')
+carro = pygame.image.load('game/carropreto.png')
+
+
+janela = pygame.display.set_mode((800,600))
+#nome da janela abaixo
+
+pygame.display.set_caption("First Game")
+
+janela_aberta = True
+while janela_aberta :
+    pygame.time.delay(50)
+#mostrar eventos
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            janela_aberta = False
+            
+    comandos = pygame.key.get_pressed()
+    if comandos[pygame.K_UP]:
+        y-= velocidade_objeto1
+    if comandos[pygame.K_DOWN]:
+        y+= velocidade_objeto1
+    if comandos[pygame.K_RIGHT]:
+        x+= velocidade_objeto1
+    if comandos[pygame.K_LEFT]:
+        x-= velocidade_objeto1 
+           
+    janela.blit(tela_fundo1, (0,0))   
+    janela.blit(carro, (x,y))
+ 
+                    
+    pygame.display.update ()  
+pygame.quit
